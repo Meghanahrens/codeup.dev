2e288419e83915b9770a6c87e6726b2abb894a77
Meghan Ahrens <meghanahrens@Meghans-MacBook-Pro.local>
2015-06-16 15:51:06 -0500

    my if else js

--- public/js/external.js
+++ public/js/external.js
@@ -1,1 +1,12 @@
 console.log('Hello from external JavaScript!');
+// If example
+if (true) {
+	console.log("This should happen");
+}
+var someBoolean = false;
+if(someBoolean) {
+	console.log("This variable is true")
+}
+else {
+	console.log("this variable is false")
+}
